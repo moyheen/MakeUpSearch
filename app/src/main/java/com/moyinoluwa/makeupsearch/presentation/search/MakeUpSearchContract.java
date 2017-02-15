b7ef8115926dd5f231bfe3ee193e7ce53fe9c9c0
Moyinoluwa <moyinoluwaa@gmail.com>
2017-02-15 23:46:51 +0100

    feat: Create Make Up Search contract

--- app/src/main/java/com/moyinoluwa/makeupsearch/presentation/search/MakeUpSearchContract.java
+++ app/src/main/java/com/moyinoluwa/makeupsearch/presentation/search/MakeUpSearchContract.java
@@ -1,8 +1,29 @@
 package com.moyinoluwa.makeupsearch.presentation.search;
 
+import com.moyinoluwa.makeupsearch.data.remote.model.MakeUpList;
+import com.moyinoluwa.makeupsearch.presentation.base.MvpPresenter;
+import com.moyinoluwa.makeupsearch.presentation.base.MvpView;
+import com.moyinoluwa.makeupsearch.presentation.select_product.SelectProductContract;
+
+import java.util.List;
+
 /**
  * Created by moyinoluwa on 2/8/17.
  */
 
 public interface MakeUpSearchContract {
+
+    interface View extends MvpView {
+        void showSearchResults(List<MakeUpList> makeUpListList);
+
+        void showError(String message);
+
+        void showLoading();
+
+        void hideLoading();
+    }
+
+    interface Presenter extends MvpPresenter<SelectProductContract.View> {
+        void search(String term);
+    }
 }
