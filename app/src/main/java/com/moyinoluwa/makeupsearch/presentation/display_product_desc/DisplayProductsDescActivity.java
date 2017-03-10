c56483ef87c74653fd645f3be48c2e6924dee059
Moyinoluwa <moyinoluwaa@gmail.com>
2017-03-10 14:09:01 +0100

    feat: Display product image in toolbar

--- app/src/main/java/com/moyinoluwa/makeupsearch/presentation/display_product_desc/DisplayProductsDescActivity.java
+++ app/src/main/java/com/moyinoluwa/makeupsearch/presentation/display_product_desc/DisplayProductsDescActivity.java
@@ -3,11 +3,20 @@ package com.moyinoluwa.makeupsearch.presentation.display_product_desc;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 import com.moyinoluwa.makeupsearch.R;
+import com.squareup.picasso.Picasso;
 
 
 public class DisplayProductsDescActivity extends AppCompatActivity {
+    ImageView imageViewToolbar;
+
+    String name;
+    String price;
+    String imageLink;
+    String productLink;
+    String description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,23 @@ public class DisplayProductsDescActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_products_desc);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle("");
 
+        imageViewToolbar = (ImageView) findViewById(R.id.toolbar_image);
+
+        retrieveDataFromIntent();
+        setToolbarImage();
+    }
+
+    private void retrieveDataFromIntent() {
+        name = getIntent().getStringExtra("name");
+        price = getIntent().getStringExtra("price");
+        imageLink = getIntent().getStringExtra("image_link");
+        productLink = getIntent().getStringExtra("product_link");
+        description = getIntent().getStringExtra("description");
+    }
+
+    private void setToolbarImage() {
+        Picasso.with(this).load(imageLink).into(imageViewToolbar);
     }
 }
