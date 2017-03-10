56af9347434f8727062e9b660520302970cf8704
Moyinoluwa <moyinoluwaa@gmail.com>
2017-03-10 15:09:06 +0100

    feat: Catch and display alternate value for null price textviews

--- app/src/main/java/com/moyinoluwa/makeupsearch/presentation/display_product_desc/DisplayProductsDescActivity.java
+++ app/src/main/java/com/moyinoluwa/makeupsearch/presentation/display_product_desc/DisplayProductsDescActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.moyinoluwa.makeupsearch.R;
 import com.squareup.picasso.Picasso;
@@ -11,6 +12,9 @@ import com.squareup.picasso.Picasso;
 
 public class DisplayProductsDescActivity extends AppCompatActivity {
     ImageView imageViewToolbar;
+    TextView textViewProductTitle;
+    TextView textViewProductPrice;
+    TextView textViewProductDescription;
 
     String name;
     String price;
@@ -22,19 +26,27 @@ public class DisplayProductsDescActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_products_desc);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setTitle("");
 
         imageViewToolbar = (ImageView) findViewById(R.id.toolbar_image);
+        textViewProductTitle = (TextView) findViewById(R.id.textView_title);
+        textViewProductPrice = (TextView) findViewById(R.id.textView_price);
+        textViewProductDescription = (TextView) findViewById(R.id.textView_description);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
 
         retrieveDataFromIntent();
         setToolbarImage();
+        setUiText();
     }
 
     private void retrieveDataFromIntent() {
         name = getIntent().getStringExtra("name");
-        price = getIntent().getStringExtra("price");
+
+        price = (price == null) ? "No price available" : getIntent().getStringExtra("price");
+
         imageLink = getIntent().getStringExtra("image_link");
         productLink = getIntent().getStringExtra("product_link");
         description = getIntent().getStringExtra("description");
@@ -42,5 +54,11 @@ public class DisplayProductsDescActivity extends AppCompatActivity {
 
     private void setToolbarImage() {
         Picasso.with(this).load(imageLink).into(imageViewToolbar);
+    }
+
+    private void setUiText() {
+        textViewProductTitle.setText(name);
+        textViewProductPrice.setText(price);
+        textViewProductDescription.setText(description);
     }
 }
